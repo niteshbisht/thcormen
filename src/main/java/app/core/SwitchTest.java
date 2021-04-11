db092998e1596b683f2b08c49e1b31611df5b48b
niteshbisht <niteshbisht98@gmail.com>
2021-04-10 22:10:09 -0400

    new code

--- src/main/java/app/core/SwitchTest.java
+++ src/main/java/app/core/SwitchTest.java
@@ -1,11 +1,15 @@
 package app.core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class SwitchTest {
   public static void main(String[] args) {
-    String c = null;
-    switch (c) {
-      default:
-        System.out.println("None");
-    }
+    List<Integer> test = IntStream.range(1, 200).boxed().collect(Collectors.toList());
+    Stream<Integer> parallel = test.stream().parallel();
+    System.out.println("parallel");
   }
 }
