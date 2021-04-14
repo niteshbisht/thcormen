f1e79a960963697abf12b621f2122d785caf4348
niteshbisht <niteshbisht98@gmail.com>
2021-04-13 23:04:20 -0400

    new files for algorithms

---
+++ src/main/java/app/core/primitives/ArrayDemo.java
@@ -0,0 +1,12 @@
+package app.core.primitives;
+
+import java.util.Arrays;
+
+public class ArrayDemo {
+  public static void main(String[] args) {
+    int[] q = {9, 2,3,4,5,7};
+    Arrays.parallelSort(q);
+
+
+  }
+}
---
+++ src/main/java/app/core/trycatchfinally/TestTryCatchFinally.java
@@ -0,0 +1,18 @@
+package app.core.trycatchfinally;
+
+public class TestTryCatchFinally {
+  public static void main(String[] args) {
+    System.out.println(testCompute());
+  }
+
+  static int testCompute() {
+    try{
+      throw new IllegalStateException();
+    } catch (Exception e) {
+      System.out.println("In Exception Area");
+      return 0;
+    } finally{
+      return -1;
+    }
+  }
+}
