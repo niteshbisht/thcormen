fb30d41ed6f75d99d501b231216364c502b8af17
niteshbisht <niteshbisht98@gmail.com>
2021-05-15 08:11:31 -0400

    changes for max consec ones soln

--- src/main/java/app/algo/arrays/MaxConsecutiveOnesII.java
+++ src/main/java/app/algo/arrays/MaxConsecutiveOnesII.java
@@ -1,7 +1,32 @@
 package app.algo.arrays;
 
-public class MaxConsecutiveOnesII {
-  public static void main(String[] args) {
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+public class MaxConsecutiveOnesII {
+
+  public static int findMaxConsecutiveOnes(int[] nums) {
+    int numZeroes = 0, left = 0, right = 0, maxConsecutiveOnes = 0;
+    while (right < nums.length) {
+      if (nums[right] == 0) {
+        numZeroes++;
+      }
+
+      while (numZeroes == 2) {
+        if (nums[left] == 0) {
+          numZeroes--;
+        }
+        left++;
+      }
+      maxConsecutiveOnes = Math.max(maxConsecutiveOnes, right - left + 1);
+      right++;
+    }
+    return maxConsecutiveOnes;
+  }
+
+  public static void main(String[] args) {
+    var data = new int[] { 1, 0, 1, 1, 0 };
+    int findMaxConsecutiveOnes = findMaxConsecutiveOnes(data);
+    log.info("value {}", findMaxConsecutiveOnes);
   }
 }
